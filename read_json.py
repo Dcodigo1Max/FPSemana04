6671395e88c20823c9367ef0c57825c49e1bdb48
Dcodigo1Max <davidrasinhas.info@gmail.com>
2024-11-29 15:15:48 +0000

    Fourth commit

---
+++ read_json.py
@@ -0,0 +1,16 @@
+#fourth commit
+import json
+
+try:
+    path =input()
+    file = open(path,"rt", encoding="utf-8")
+    json_data = file.read() 
+    data = json.loads(json_data) 
+    print("caminho_certo/data.json")
+    print(data) 
+    file.close()
+except:
+    print("caminho errado/data.json")
+    print("Ocorreu um erro!")
+finally:
+    print("Processo Concluido!")
