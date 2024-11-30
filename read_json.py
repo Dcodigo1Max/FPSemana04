da39777e91b205f6098d640ecd9f40d13e2db6b1
Dcodigo1Max <davidrasinhas.info@gmail.com>
2024-11-30 11:27:18 +0000

    Last commit

--- read_json.py
+++ read_json.py
@@ -2,7 +2,7 @@ import json
 
 try:
     path =input()
-    file = open(path,"rt", encoding="utf-8")
+    file = open(path, "rt", encoding= "utf-8")
     json_data = file.read() 
     data = json.loads(json_data) 
     print("caminho_certo/data.json")
